8198978be15b3804a54c07f16c2137e91c278720
HewD77 <162551520+HewD77@users.noreply.github.com>
2025-02-27 14:04:40 +0800

    fix, userprofile bug

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -78,7 +78,13 @@ const registerUser = async (req, res) => {
 
         const token = createToken(user._id)
 
-        res.json({ success: true, token })
+        res.json({
+            success: true, token, user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+            }
+        })
 
     } catch (error) {
         console.log(error);
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -20,6 +20,7 @@ const Login = () => {
         const response = await axios.post(backendUrl + '/api/user/register', { name, email, password })
         if (response.data.success) {
           setToken(response.data.token)
+          localStorage.setItem('user', JSON.stringify(response.data.user));
           localStorage.setItem('token', response.data.token)
         } else {
           toast.error(response.data.message)
